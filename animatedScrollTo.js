39450e95e467b6076a9fb8e68a7ad725e1f31da0
Valterson Filho <valterson@enesolucoes.com.br>
2017-08-02 11:24:58 -0300

    fix window undefined.

--- animatedScrollTo.js
+++ animatedScrollTo.js
@@ -1,50 +1,52 @@
-(function (window) {
-    var requestAnimFrame = (function(){return window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||function(callback){window.setTimeout(callback,1000/60);};})();
+if (typeof window !== 'undefined') {
+    (function (window) {
+        var requestAnimFrame = (function(){return window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||function(callback){window.setTimeout(callback,1000/60);};})();
 
-    var easeInOutQuad = function (t, b, c, d) {
-        t /= d/2;
-        if (t < 1) return c/2*t*t + b;
-        t--;
-        return -c/2 * (t*(t-2) - 1) + b;
-    };
+        var easeInOutQuad = function (t, b, c, d) {
+            t /= d/2;
+            if (t < 1) return c/2*t*t + b;
+            t--;
+            return -c/2 * (t*(t-2) - 1) + b;
+        };
 
-    var animatedScrollTo = function (element, to, duration, callback) {
-        var start = element.scrollTop,
-        change = to - start,
-        animationStart = +new Date();
-        var animating = true;
-        var lastpos = null;
+        var animatedScrollTo = function (element, to, duration, callback) {
+            var start = element.scrollTop,
+            change = to - start,
+            animationStart = +new Date();
+            var animating = true;
+            var lastpos = null;
 
-        var animateScroll = function() {
-            if (!animating) {
-                return;
-            }
-            requestAnimFrame(animateScroll);
-            var now = +new Date();
-            var val = Math.floor(easeInOutQuad(now - animationStart, start, change, duration));
-            if (lastpos) {
-                if (lastpos === element.scrollTop) {
+            var animateScroll = function() {
+                if (!animating) {
+                    return;
+                }
+                requestAnimFrame(animateScroll);
+                var now = +new Date();
+                var val = Math.floor(easeInOutQuad(now - animationStart, start, change, duration));
+                if (lastpos) {
+                    if (lastpos === element.scrollTop) {
+                        lastpos = val;
+                        element.scrollTop = val;
+                    } else {
+                        animating = false;
+                    }
+                } else {
                     lastpos = val;
                     element.scrollTop = val;
-                } else {
-                    animating = false;
                 }
-            } else {
-                lastpos = val;
-                element.scrollTop = val;
-            }
-            if (now > animationStart + duration) {
-                element.scrollTop = to;
-                animating = false;
-                if (callback) { callback(); }
-            }
+                if (now > animationStart + duration || element.scrollTop == to) {
+                    element.scrollTop = to;
+                    animating = false;
+                    if (callback) { callback(); }
+                }
+            };
+            requestAnimFrame(animateScroll);
         };
-        requestAnimFrame(animateScroll);
-    };
 
-    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-        module.exports = animatedScrollTo;
-    } else {
-        window.animatedScrollTo = animatedScrollTo;
-    }
-})(window);
+        if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+            module.exports = animatedScrollTo;
+        } else {
+            window.animatedScrollTo = animatedScrollTo;
+        }
+    })(window);
+}
